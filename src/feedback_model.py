8792b508c5f4c04b58c64f4295bb6f4dae4e32c9
Kasper Demeyere <kasperdemeyere@hotmail.com>
2024-02-23 11:16:09 +0100

    move subtree_matches to own file

--- src/feedback_model.py
+++ src/feedback_model.py
@@ -11,7 +11,7 @@ from src.analyze import FeedbackAnalyzer
 from src.custom_types import AnnotatedTree, Tree, HorizontalTree, PatternCollection
 from src.util import to_string_encoding
 from src.tree_algorithms.treeminer import Treeminerd
-from src.util import sequence_fully_contains_other_sequence
+from src.tree_algorithms.subtree_matches import subtree_matches
 from src.tree_algorithms.subtree_on_line import find_subtree_on_line
 
 
@@ -111,134 +111,9 @@ class FeedbackModel:
 
         print(f"Total training time: {datetime.datetime.now() - start}")
 
-    @staticmethod
-    def subtree_matches(subtree: HorizontalTree, pattern: HorizontalTree) -> bool:
-        """
-        >>> model = FeedbackModel()
-        >>> model.subtree_matches((0, 1, 3, 7, -1, 2, -1, -1, 2, -1, -1, 2, -1), (0, 1, -1, 2, -1, 2, -1, 2, -1)) # Fig 2 modified
-        False
-        >>> model.subtree_matches((0, 1, 3, 1, -1, 2, -1, -1, 2, -1, -1, 2, -1), (1, 2, -1, 1, -1)) # Fig 2
-        False
-        >>> model.subtree_matches((0, 1, 3, 1, -1, 2, -1, -1, 2, -1, -1, 1, -1), (1, 2, -1, 1, -1)) # Fig 2 modified
-        False
-        >>> model.subtree_matches((0, 1, 3, 1, -1, 2, -1, -1, 2, -1, -1, 2, -1), (1, 1, -1, 2, -1)) # Fig 2
-        True
-        >>> model.subtree_matches((0, 1, 3, 1, -1, 4, -1, -1, 4, -1, -1, 2, -1), (1, 1, -1, 2, -1)) # Fig 2 modified
-        False
-        >>> model.subtree_matches((0, 1, 3, 1, -1, 2, -1, -1, 2, -1, -1, 2, -1), (0, 1, -1, 2, -1, 2, -1, 2, -1)) # Fig 2
-        True
-        >>> model.subtree_matches((0, 1, 3, 1, 2, -1, -1, 4, -1, -1, 4, -1, -1, 4, -1), (1, 1, -1, 2, -1))
-        False
-        >>> model.subtree_matches((1, 1, 2, -1, -1, 1, 2, -1, -1), (1, 1, -1, 1, -1))
-        True
-        >>> model.subtree_matches((1, 1, 2, -1, -1, 1, 2, -1, -1), (1, 1, -1, 1, 2, -1, -1))
-        True
-        >>> model.subtree_matches((1, 1, 2, -1, -1, 1, 2, -1, -1), (1, 1, -1, 1, -1, 2, -1))
-        False
-        >>> model.subtree_matches((0, 3, 4, -1, 5, -1, 6, 7, -1, 8, -1, 2, -1, -1, 9, -1, 1, 10, 11, 12, -1, -1, -1, -1, -1), (0, 1, 2, -1, -1))
-        False
-        >>> model.subtree_matches((0, 3, 4, -1, 5, -1, 6, 7, -1, 8, -1, 2, -1, -1, 9, -1, 1, 10, 11, 12, -1, -1, -1, -1, -1), (13,))
-        False
-        >>> model.subtree_matches((1, 2, -1, 3, 4, -1, -1), (1, 2, -1, 4, -1)) # Fig 5 T0
-        True
-        >>> model.subtree_matches((2, 1, 2, -1, 4, -1, -1, 2, -1, 3, -1), (1, 2, -1, 4, -1)) # Fig 5 T1
-        True
-        >>> model.subtree_matches((1, 3, 2, -1, -1, 5, 1, 2, -1, 3, 4, -1, -1, -1, -1), (1, 2, -1, 4, -1)) # Fig 5 T2
-        True
-        >>> model.subtree_matches((0, 1, 2, 3, -1, -1, 2, -1, 4, -1, -1, 1, 2, -1, -1), (0, 1, 3, -1, 4, -1, -1))
-        True
-        >>> model.subtree_matches((0, 1, 2, 3, -1, -1, 2, -1, 5, -1, -1, 1, 4, -1, -1), (0, 1, 3, -1, 4, -1, -1))
-        False
-        >>> model.subtree_matches((0, 1, 3, 1, -1, 4, -1, -1, 5, -1, -1, 6, -1, 1, 2, -1, -1), (0, 1, 2, -1, -1))
-        True
-        >>> model.subtree_matches((4, 0, 1, 3, 1, -1, 2, -1, -1, 2, -1, -1, 2, -1, -1, 5, -1), (0, 1, -1, 2, -1, 2, -1, 2, -1))
-        True
-        >>> model.subtree_matches(('expression_statement', 'assignment', 'uitgang', -1, '=', -1, 'list', '[', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ']', -1, -1, -1), ('list', 'call', 'range', -1, '(', -1, '('))
-        False
-        >>> model.subtree_matches(('expression_statement', 'augmented_assignment', 'output', -1, '+=', -1, 'subscript', 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'attribute', 'attribute', 'self', -1, '.', -1, 'codec', -1, -1, '.', -1, 'keys', -1, -1, 'argument_list', '(', -1, ')', -1, -1, -1, ')', -1, -1, -1, '[', -1, 'call', 'attribute', 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'attribute', 'attribute', 'self', -1, '.', -1, 'codec', -1, -1, '.', -1, 'values', -1, -1, 'argument_list', '(', -1, ')', -1, -1, -1, ')', -1, -1, -1, '.', -1, 'index', -1, -1, 'argument_list', '(', -1, 'element', -1, ')', -1, -1, -1, ']', -1, -1, -1), ('call', 'attribute', '.', -1, -1, '(', -1, 'self'))
-        False
-        >>> model.subtree_matches(('function_definition', 'def', -1, 'verborgen_coderen', -1, 'parameters', '(', -1, 'self', -1, ',', -1, 'klare_tekst', -1, ',', -1, 'default_parameter', 'a', -1, '=', -1, 'none', -1, -1, ',', -1, 'something', 'b', -1, '=', -1, 'none', -1, -1, ')', -1, -1, ':', -1, 'block', -1), ('function_definition', 'def', -1, 'parameters', '(', -1, 'default_parameter', 'b', -1, '=', -1, 'none', -1, -1, -1, 'block'))
-        False
-        >>> model.subtree_matches(('function_definition', 'def', -1, 'verborgen_coderen', -1, 'parameters', '(', -1, 'self', -1, ',', -1, 'klare_tekst', -1, ',', -1, 'default_parameter', 'a', -1, '=', -1, 'none', -1, -1, ',', -1, 'default_parameter', 'b', -1, '=', -1, 'none', -1, -1, ')', -1, -1, ':', -1, 'block', -1), ('parameters', '(', -1, 'self', -1, ',', -1, 'default_parameter', 'b', -1, -1, ')'))
-        True
-        >>> model.subtree_matches(('function_definition', 'def', -1, 'verborgen_coderen', -1, 'parameters', '(', -1, 'self', -1, ',', -1, 'klare_tekst', -1, ',', -1, 'default_parameter', 'a', -1, '=', -1, 'none', -1, -1, ',', -1, 'default_parameter', 'b', -1, '=', -1, 'none', -1, -1, ')', -1, -1, ':', -1, 'block', -1), ('function_definition', 'def', -1, 'parameters', '(', -1, 'default_parameter', 'b', -1, '=', -1, 'none', -1, -1, -1, 'block'))
-        True
-        >>> model.subtree_matches(('function_definition', 'def', -1, 'verborgen_coderen', -1, 'parameters', '(', -1, 'self', -1, ',', -1, 'klare_tekst', -1, ',', -1, 'default_parameter', 'a', -1, '=', -1, 'none', -1, -1, ',', -1, 'default_parameter', 'b', -1, '=', -1, 'none', -1, -1, ')', -1, -1, ':', -1, 'block', -1), ('function_definition', 'def', -1, 'parameters', '(', -1, ',', -1, 'default_parameter', 'b', -1, '=', -1, 'none', -1, -1, -1, ':'))
-        True
-        >>> model.subtree_matches(('expression_statement', 'assignment', 'lijst', -1, '=', -1, 'list_comprehension', '[', -1, 'call', 'read_cocktail', -1, 'argument_list', '(', -1, 'line', -1, ')', -1, -1, -1, 'for_in_clause', 'for', -1, 'line', -1, 'in', -1, 'call', 'map', -1, 'argument_list', '(', -1, 'attribute', 'str', -1, '.', -1, 'strip', -1, -1, ',', -1, 'call', 'open', -1, 'argument_list', '(', -1, 'file', -1, ',', -1, 'string', '"', -1, '"', -1, -1, ',', -1, 'keyword_argument', 'encoding', -1, '=', -1, 'string', '"', -1, '"', -1, -1, -1, ')', -1, -1, -1, ')', -1, -1, -1, -1, ']', -1, -1, -1), ('expression_statement', 'assignment', '=', -1, 'call', 'argument_list', '(', -1, ',', -1, '"', -1, '"'))
-        True
-        >>> model.subtree_matches(('expression_statement', 'assignment', 'lijst', -1, '=', -1, 'list_comprehension', '[', -1, 'call', 'read_cocktail', -1, 'argument_list', '(', -1, 'line', -1, ')', -1, -1, -1, 'for_in_clause', 'for', -1, 'line', -1, 'in', -1, 'call', 'map', -1, 'argument_list', '(', -1, 'attribute', 'str', -1, '.', -1, 'strip', -1, -1, ',', -1, 'call', 'open', -1, 'argument_list', '(', -1, 'file', -1, ',', -1, 'string', '"', -1, '"', -1, -1, ',', -1, 'keyword_argument', 'encoding', -1, '=', -1, 'string', '"', -1, '"', -1, -1, -1, ')', -1, -1, -1, ')', -1, -1, -1, -1, ']', -1, -1, -1), ('expression_statement', 'assignment', 'call', 'argument_list', 'string'))
-        True
-        >>> model.subtree_matches(('expression_statement', 'assignment', 'lijst', -1, '=', -1, 'list_comprehension', '[', -1, 'call', 'read_cocktail', -1, 'argument_list', '(', -1, 'line', -1, ')', -1, -1, -1, 'for_in_clause', 'for', -1, 'line', -1, 'in', -1, 'call', 'map', -1, 'argument_list', '(', -1, 'attribute', 'str', -1, '.', -1, 'strip', -1, -1, ',', -1, 'call', 'open', -1, 'argument_list', '(', -1, 'file', -1, ',', -1, 'string', '"', -1, '"', -1, -1, ',', -1, 'keyword_argument', 'encoding', -1, '=', -1, 'string', '"', -1, '"', -1, -1, -1, ')', -1, -1, -1, ')', -1, -1, -1, -1, ']', -1, -1, -1), ('expression_statement', 'assignment', 'call', 'argument_list', '(', -1, ','))
-        True
-        >>> model.subtree_matches(('expression_statement', 'assignment', 'uitgang', -1, '=', -1, 'list', '[', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ']', -1, -1, -1), ('call', 'range', -1, '(', -1, '('))
-        False
-        """
-        if not set(pattern).issubset(subtree):
-            return False
-
-        pattern_length = len(pattern)
-
-        start = 0
-        p_i = 0
-        depth = 0
-        depth_stack = []
-        history = []
-
-        def find_in_subtree(current_subtree) -> bool:
-            nonlocal start, p_i, depth, depth_stack
-
-            local_history = []
-            for i, item in enumerate(current_subtree):
-                # We go up in the tree
-                if item == -1:
-                    # If the depth of the last matched node is equal to what the depth will be after this iteration
-                    if depth_stack and depth - 1 == depth_stack[-1]:
-                        # We go up past the last node
-                        depth_stack.pop()
-                        # If in the pattern, we are not supposed to go up
-                        if pattern[p_i] != -1:
-                            # Reset the pattern index
-                            p_i = 0
-                            # If we are past the first matched node, we can limit our future backtracking in local_history to a new subtree
-                            if not depth_stack:
-                                history.append((local_history, current_subtree[:i + 1]))
-                                local_history = []
-                        else:
-                            p_i += 1
-                    depth -= 1
-                else:
-                    if pattern[p_i] == item:
-                        local_history.append((start + i + 1, depth + 1, depth_stack[:], p_i))
-                        depth_stack.append(depth)
-                        p_i += 1
-
-                    depth += 1
-
-                if p_i == pattern_length:
-                    return True
-
-            if local_history:
-                history.append((local_history, current_subtree))
-
-            return False
-
-        result = find_in_subtree(subtree)
-        while not result and history:
-            to_explore, to_explore_subtree = history.pop()
-            while not result and to_explore:
-                start, depth, depth_stack, p_i = to_explore.pop()
-                new_subtree = to_explore_subtree[start:]
-                start = 0
-                # Subtree needs to contain all items from pattern
-                if pattern_length - p_i <= len(new_subtree) and sequence_fully_contains_other_sequence(new_subtree, pattern[p_i:]):
-                    result = find_in_subtree(new_subtree)
-
-        return result
-
     def calculate_matching_score(self, m: str, subtree: HorizontalTree) -> float:
         pattern_set = self.patterns[m][0]
-        matches = list(filter(lambda pattern: self.subtree_matches(subtree, pattern), pattern_set))
+        matches = list(filter(lambda pattern: subtree_matches(subtree, pattern), pattern_set))
         matches_score = 0
         if pattern_set:
             matches_score = sum(self.pattern_weights[match] for match in matches) / len(pattern_set)
---
+++ src/tree_algorithms/subtree_matches.py
@@ -0,0 +1,126 @@
+from src.custom_types import HorizontalTree
+from src.util import sequence_fully_contains_other_sequence
+
+
+def subtree_matches(subtree: HorizontalTree, pattern: HorizontalTree) -> bool:
+    """
+    >>> subtree_matches((0, 1, 3, 7, -1, 2, -1, -1, 2, -1, -1, 2, -1), (0, 1, -1, 2, -1, 2, -1, 2, -1)) # Fig 2 modified
+    False
+    >>> subtree_matches((0, 1, 3, 1, -1, 2, -1, -1, 2, -1, -1, 2, -1), (1, 2, -1, 1, -1)) # Fig 2
+    False
+    >>> subtree_matches((0, 1, 3, 1, -1, 2, -1, -1, 2, -1, -1, 1, -1), (1, 2, -1, 1, -1)) # Fig 2 modified
+    False
+    >>> subtree_matches((0, 1, 3, 1, -1, 2, -1, -1, 2, -1, -1, 2, -1), (1, 1, -1, 2, -1)) # Fig 2
+    True
+    >>> subtree_matches((0, 1, 3, 1, -1, 4, -1, -1, 4, -1, -1, 2, -1), (1, 1, -1, 2, -1)) # Fig 2 modified
+    False
+    >>> subtree_matches((0, 1, 3, 1, -1, 2, -1, -1, 2, -1, -1, 2, -1), (0, 1, -1, 2, -1, 2, -1, 2, -1)) # Fig 2
+    True
+    >>> subtree_matches((0, 1, 3, 1, 2, -1, -1, 4, -1, -1, 4, -1, -1, 4, -1), (1, 1, -1, 2, -1))
+    False
+    >>> subtree_matches((1, 1, 2, -1, -1, 1, 2, -1, -1), (1, 1, -1, 1, -1))
+    True
+    >>> subtree_matches((1, 1, 2, -1, -1, 1, 2, -1, -1), (1, 1, -1, 1, 2, -1, -1))
+    True
+    >>> subtree_matches((1, 1, 2, -1, -1, 1, 2, -1, -1), (1, 1, -1, 1, -1, 2, -1))
+    False
+    >>> subtree_matches((0, 3, 4, -1, 5, -1, 6, 7, -1, 8, -1, 2, -1, -1, 9, -1, 1, 10, 11, 12, -1, -1, -1, -1, -1), (0, 1, 2, -1, -1))
+    False
+    >>> subtree_matches((0, 3, 4, -1, 5, -1, 6, 7, -1, 8, -1, 2, -1, -1, 9, -1, 1, 10, 11, 12, -1, -1, -1, -1, -1), (13,))
+    False
+    >>> subtree_matches((1, 2, -1, 3, 4, -1, -1), (1, 2, -1, 4, -1)) # Fig 5 T0
+    True
+    >>> subtree_matches((2, 1, 2, -1, 4, -1, -1, 2, -1, 3, -1), (1, 2, -1, 4, -1)) # Fig 5 T1
+    True
+    >>> subtree_matches((1, 3, 2, -1, -1, 5, 1, 2, -1, 3, 4, -1, -1, -1, -1), (1, 2, -1, 4, -1)) # Fig 5 T2
+    True
+    >>> subtree_matches((0, 1, 2, 3, -1, -1, 2, -1, 4, -1, -1, 1, 2, -1, -1), (0, 1, 3, -1, 4, -1, -1))
+    True
+    >>> subtree_matches((0, 1, 2, 3, -1, -1, 2, -1, 5, -1, -1, 1, 4, -1, -1), (0, 1, 3, -1, 4, -1, -1))
+    False
+    >>> subtree_matches((0, 1, 3, 1, -1, 4, -1, -1, 5, -1, -1, 6, -1, 1, 2, -1, -1), (0, 1, 2, -1, -1))
+    True
+    >>> subtree_matches((4, 0, 1, 3, 1, -1, 2, -1, -1, 2, -1, -1, 2, -1, -1, 5, -1), (0, 1, -1, 2, -1, 2, -1, 2, -1))
+    True
+    >>> subtree_matches(('expression_statement', 'assignment', 'uitgang', -1, '=', -1, 'list', '[', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ']', -1, -1, -1), ('list', 'call', 'range', -1, '(', -1, '('))
+    False
+    >>> subtree_matches(('expression_statement', 'augmented_assignment', 'output', -1, '+=', -1, 'subscript', 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'attribute', 'attribute', 'self', -1, '.', -1, 'codec', -1, -1, '.', -1, 'keys', -1, -1, 'argument_list', '(', -1, ')', -1, -1, -1, ')', -1, -1, -1, '[', -1, 'call', 'attribute', 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'attribute', 'attribute', 'self', -1, '.', -1, 'codec', -1, -1, '.', -1, 'values', -1, -1, 'argument_list', '(', -1, ')', -1, -1, -1, ')', -1, -1, -1, '.', -1, 'index', -1, -1, 'argument_list', '(', -1, 'element', -1, ')', -1, -1, -1, ']', -1, -1, -1), ('call', 'attribute', '.', -1, -1, '(', -1, 'self'))
+    False
+    >>> subtree_matches(('function_definition', 'def', -1, 'verborgen_coderen', -1, 'parameters', '(', -1, 'self', -1, ',', -1, 'klare_tekst', -1, ',', -1, 'default_parameter', 'a', -1, '=', -1, 'none', -1, -1, ',', -1, 'something', 'b', -1, '=', -1, 'none', -1, -1, ')', -1, -1, ':', -1, 'block', -1), ('function_definition', 'def', -1, 'parameters', '(', -1, 'default_parameter', 'b', -1, '=', -1, 'none', -1, -1, -1, 'block'))
+    False
+    >>> subtree_matches(('function_definition', 'def', -1, 'verborgen_coderen', -1, 'parameters', '(', -1, 'self', -1, ',', -1, 'klare_tekst', -1, ',', -1, 'default_parameter', 'a', -1, '=', -1, 'none', -1, -1, ',', -1, 'default_parameter', 'b', -1, '=', -1, 'none', -1, -1, ')', -1, -1, ':', -1, 'block', -1), ('parameters', '(', -1, 'self', -1, ',', -1, 'default_parameter', 'b', -1, -1, ')'))
+    True
+    >>> subtree_matches(('function_definition', 'def', -1, 'verborgen_coderen', -1, 'parameters', '(', -1, 'self', -1, ',', -1, 'klare_tekst', -1, ',', -1, 'default_parameter', 'a', -1, '=', -1, 'none', -1, -1, ',', -1, 'default_parameter', 'b', -1, '=', -1, 'none', -1, -1, ')', -1, -1, ':', -1, 'block', -1), ('function_definition', 'def', -1, 'parameters', '(', -1, 'default_parameter', 'b', -1, '=', -1, 'none', -1, -1, -1, 'block'))
+    True
+    >>> subtree_matches(('function_definition', 'def', -1, 'verborgen_coderen', -1, 'parameters', '(', -1, 'self', -1, ',', -1, 'klare_tekst', -1, ',', -1, 'default_parameter', 'a', -1, '=', -1, 'none', -1, -1, ',', -1, 'default_parameter', 'b', -1, '=', -1, 'none', -1, -1, ')', -1, -1, ':', -1, 'block', -1), ('function_definition', 'def', -1, 'parameters', '(', -1, ',', -1, 'default_parameter', 'b', -1, '=', -1, 'none', -1, -1, -1, ':'))
+    True
+    >>> subtree_matches(('expression_statement', 'assignment', 'lijst', -1, '=', -1, 'list_comprehension', '[', -1, 'call', 'read_cocktail', -1, 'argument_list', '(', -1, 'line', -1, ')', -1, -1, -1, 'for_in_clause', 'for', -1, 'line', -1, 'in', -1, 'call', 'map', -1, 'argument_list', '(', -1, 'attribute', 'str', -1, '.', -1, 'strip', -1, -1, ',', -1, 'call', 'open', -1, 'argument_list', '(', -1, 'file', -1, ',', -1, 'string', '"', -1, '"', -1, -1, ',', -1, 'keyword_argument', 'encoding', -1, '=', -1, 'string', '"', -1, '"', -1, -1, -1, ')', -1, -1, -1, ')', -1, -1, -1, -1, ']', -1, -1, -1), ('expression_statement', 'assignment', '=', -1, 'call', 'argument_list', '(', -1, ',', -1, '"', -1, '"'))
+    True
+    >>> subtree_matches(('expression_statement', 'assignment', 'lijst', -1, '=', -1, 'list_comprehension', '[', -1, 'call', 'read_cocktail', -1, 'argument_list', '(', -1, 'line', -1, ')', -1, -1, -1, 'for_in_clause', 'for', -1, 'line', -1, 'in', -1, 'call', 'map', -1, 'argument_list', '(', -1, 'attribute', 'str', -1, '.', -1, 'strip', -1, -1, ',', -1, 'call', 'open', -1, 'argument_list', '(', -1, 'file', -1, ',', -1, 'string', '"', -1, '"', -1, -1, ',', -1, 'keyword_argument', 'encoding', -1, '=', -1, 'string', '"', -1, '"', -1, -1, -1, ')', -1, -1, -1, ')', -1, -1, -1, -1, ']', -1, -1, -1), ('expression_statement', 'assignment', 'call', 'argument_list', 'string'))
+    True
+    >>> subtree_matches(('expression_statement', 'assignment', 'lijst', -1, '=', -1, 'list_comprehension', '[', -1, 'call', 'read_cocktail', -1, 'argument_list', '(', -1, 'line', -1, ')', -1, -1, -1, 'for_in_clause', 'for', -1, 'line', -1, 'in', -1, 'call', 'map', -1, 'argument_list', '(', -1, 'attribute', 'str', -1, '.', -1, 'strip', -1, -1, ',', -1, 'call', 'open', -1, 'argument_list', '(', -1, 'file', -1, ',', -1, 'string', '"', -1, '"', -1, -1, ',', -1, 'keyword_argument', 'encoding', -1, '=', -1, 'string', '"', -1, '"', -1, -1, -1, ')', -1, -1, -1, ')', -1, -1, -1, -1, ']', -1, -1, -1), ('expression_statement', 'assignment', 'call', 'argument_list', '(', -1, ','))
+    True
+    >>> subtree_matches(('expression_statement', 'assignment', 'uitgang', -1, '=', -1, 'list', '[', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ']', -1, -1, -1), ('call', 'range', -1, '(', -1, '('))
+    False
+    """
+    if not set(pattern).issubset(subtree):
+        return False
+
+    pattern_length = len(pattern)
+
+    start = 0
+    p_i = 0
+    depth = 0
+    depth_stack = []
+    history = []
+
+    def find_in_subtree(current_subtree) -> bool:
+        nonlocal start, p_i, depth, depth_stack
+
+        local_history = []
+        for i, item in enumerate(current_subtree):
+            # We go up in the tree
+            if item == -1:
+                # If the depth of the last matched node is equal to what the depth will be after this iteration
+                if depth_stack and depth - 1 == depth_stack[-1]:
+                    # We go up past the last node
+                    depth_stack.pop()
+                    # If in the pattern, we are not supposed to go up
+                    if pattern[p_i] != -1:
+                        # Reset the pattern index
+                        p_i = 0
+                        # If we are past the first matched node, we can limit our future backtracking in local_history to a new subtree
+                        if not depth_stack:
+                            history.append((local_history, current_subtree[:i + 1]))
+                            local_history = []
+                    else:
+                        p_i += 1
+                depth -= 1
+            else:
+                if pattern[p_i] == item:
+                    local_history.append((start + i + 1, depth + 1, depth_stack[:], p_i))
+                    depth_stack.append(depth)
+                    p_i += 1
+
+                depth += 1
+
+            if p_i == pattern_length:
+                return True
+
+        if local_history:
+            history.append((local_history, current_subtree))
+
+        return False
+
+    result = find_in_subtree(subtree)
+    while not result and history:
+        to_explore, to_explore_subtree = history.pop()
+        while not result and to_explore:
+            start, depth, depth_stack, p_i = to_explore.pop()
+            new_subtree = to_explore_subtree[start:]
+            start = 0
+            # Subtree needs to contain all items from pattern
+            if pattern_length - p_i <= len(new_subtree) and sequence_fully_contains_other_sequence(new_subtree, pattern[p_i:]):
+                result = find_in_subtree(new_subtree)
+
+    return result
