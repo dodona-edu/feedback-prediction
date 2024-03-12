5ab8c9f9acde0c9dc35f9382e7c562ca7dcc8cb5
Kasper Demeyere <kasperdemeyere@hotmail.com>
2024-03-12 17:39:00 +0100

    clean up predictor code

--- src/predictor.py
+++ src/predictor.py
@@ -1,171 +1,142 @@
-import math
 import csv
+import json
+import math
+import time
+from collections import Counter, defaultdict
 from glob import glob
-from typing import List, Tuple
-from collections import defaultdict, Counter
+from typing import List, Tuple, Dict, Set
+
+from pqdm.processes import pqdm
+import multiprocessing.pool
 
 from analyze import FeedbackAnalyzer
 from constants import ROOT_DIR
+from custom_types import LineTree, Annotation, AnnotatedTree, Tree
 from feedback_model import FeedbackModel
-from custom_types import AnnotatedTree
 from tree_algorithms.subtree_on_line import find_subtree_on_line
 
 
-message_occurrence_counter = Counter()
+class Predictor:
 
+    def __init__(self):
+        self.retrain_every_n = 5
+        self.train = {}
+        self.model = FeedbackModel()
+        self.average_occurrences_per_file = {}
 
-def save_results(results: List[Tuple[str, List[Tuple[int, List[str], str | None]]]]):
-    with open(f'{ROOT_DIR}/output/predictor/results.csv', 'w') as csv_file:
-        writer = csv.writer(csv_file, delimiter='|')
-        writer.writerow(['Filename', 'Line', 'Actual messages', 'Predicted message'])
-        for file, line_results in results:
-            for line, actual_messages, predicted_message in line_results:
-                writer.writerow([file, line, actual_messages, predicted_message])
-
-
-def most_fitting_predictions_for_file(model: FeedbackModel, annotated_tree: AnnotatedTree, stats: Counter):
-    """
-    Determine for each feedback message the most fitting line to assign it to in the file
-    """
-    # TODO average occurrences per file bijhouden, en dan per message de x beste posities aanraden (met x het aantal average occurrences)
-    tree, annotations = annotated_tree
-
-    messages_per_line = defaultdict(set)
-    for (message, line) in annotations:
-        messages_per_line[line].add(message)
-
-    subtrees_per_line = {}
-
-    matching_scores_per_line_per_message = defaultdict(dict)
-    highest_score_and_line_per_message = defaultdict(lambda: (-math.inf, -1))
-    for line in tree['lines']:
+    def _calculate_matching_scores(self, line, tree):
         subtree = find_subtree_on_line(tree, line)
-        subtrees_per_line[line] = subtree
         if subtree is not None:
-            matching_scores = model.calculate_matching_scores(subtree, identifying_only=True)
-            matching_scores_per_line_per_message[line] = matching_scores
-            over_threshold_scores = {m: s for m, s in matching_scores.items() if s >= model.score_thresholds[m]}
-            for message, score in over_threshold_scores.items():
-                if score > highest_score_and_line_per_message[message][0]:
-                    highest_score_and_line_per_message[message] = (score, line)
+            return line, self.model.calculate_matching_scores(subtree, identifying_only=True)
 
-    predicted_messages_and_score_per_line = defaultdict(dict)
-    for message, (score, line) in highest_score_and_line_per_message.items():
-        predicted_messages_and_score_per_line[line][message] = score
+        return line, None
 
-    for line, subtree in subtrees_per_line.items():
-        if subtree is not None:
-            messages = messages_per_line[line]
-            if messages:
-                print(f"Actual messages on line {line}: {list(messages)}")
+    def predict(self, tree: LineTree) -> Tuple[Dict[int, Set[str]], Dict[int, Dict[str, float]], List[int]]:
+        # Keep track of which lines had a subtree on them, only used for calculating statistics
+        lines_with_subtree = []
 
-            predicted_messages = set(predicted_messages_and_score_per_line[line].keys())
-            if predicted_messages:
-                print(f"Predicted messages on line {line}: {predicted_messages}")
+        matching_scores_per_line_per_message = defaultdict(dict)
+        scores_and_lines_per_message = defaultdict(list)
 
-            # TODO als meerdere messages predicted/on_line zijn: voor allemaal de stats aanpassen, eerste false negative niet meerekenen
-            if predicted_messages and predicted_messages.intersection(messages):
-                stats["TP"] += 1
-            elif predicted_messages and messages:
-                stats["different_message"] += 1
-            elif predicted_messages:
-                for message in predicted_messages:
-                    model.score_thresholds[message] = 1.01 * predicted_messages_and_score_per_line[line][message]
-                stats["FP"] += 1
-            elif messages:
-                for message in messages:
-                    matching_scores = matching_scores_per_line_per_message[line]
-                    if message in matching_scores:
-                        model.update_score_threshold(message, matching_scores[message])
-                stats["FN"] += 1
-                if any(m in model.patterns for m in messages):
-                    stats["filtered_FN"] += 1
-            else:
-                stats["TN"] += 1
+        pool = multiprocessing.pool.Pool(8)
+        results = pool.starmap(self._calculate_matching_scores, [(line, tree) for line in tree['lines']], chunksize=math.ceil(len(tree['lines']) / 8))
+        pool.close()
+        # results = pqdm([(line, tree) for line in tree['lines']], self._calculate_over_threshold_scores, n_jobs=8, argument_type='args', exception_behaviour='immediate')
 
+        for line, matching_scores in results:
+            if matching_scores is not None:
+                lines_with_subtree.append(line)
+                matching_scores_per_line_per_message[line] = matching_scores
+                over_threshold_scores = {m: s for m, s in matching_scores.items() if s >= self.model.score_thresholds[m]}
+                for message, score in over_threshold_scores.items():
+                    scores_and_lines_per_message[message].append((score, line))
 
-def predictions_for_file(model: FeedbackModel, annotated_tree: AnnotatedTree, stats: Counter) -> List[Tuple[int, List[str], str | None]]:
-    """
-    Predict in the file for each line the highest matching feedback message, if the matching score is above a threshold
-    """
-    tree, annotations = annotated_tree
+        predicted_messages_per_line = defaultdict(set)
+        for message, possible_annotations in scores_and_lines_per_message.items():
+            possible_annotations.sort(key=lambda an: an[0], reverse=True)
+            average_occurrences = max(round(self.average_occurrences_per_file.get(message, (0.0, 0))[0]), 1)
+            for (score, line) in possible_annotations[:average_occurrences]:
+                predicted_messages_per_line[line].add(message)
 
-    messages_per_line = defaultdict(list)
-    for (message, line) in annotations:
-        messages_per_line[line].append(message)
+        return predicted_messages_per_line, matching_scores_per_line_per_message, lines_with_subtree
 
-    results = []
-    new_score_thresholds = {}
+    def reject_suggestion(self, suggestion: Annotation, subtree: Tree):
+        # TODO negative patterns
+        pass
 
-    for line in tree['lines']:
-        predicted_message = None
-        matching_scores = []
-        subtree = find_subtree_on_line(tree, line)
-        messages_on_line = messages_per_line[line]
+    def process_actual_feedback(self, annotations: List[Annotation], predicted_messages_per_line: Dict[int, Set[str]],
+                                matching_scores_per_line_per_message: Dict[int, Dict[str, float]]) -> Dict[int, Set[str]]:
+        message_occurrences_in_this_file = Counter()
+        messages_per_line = defaultdict(set)
+        for (message, line) in annotations:
+            message_occurrences_in_this_file[message] += 1
+            messages_per_line[line].add(message)
 
-        if subtree is not None:
-            matching_scores = model.calculate_matching_scores(subtree, identifying_only=True)
-            messages_sorted = sorted(matching_scores.keys(), key=lambda ms: matching_scores[ms], reverse=True)
-            predicted_message = next((m for m in messages_sorted if matching_scores[m] >= model.score_thresholds[m]), None)
-
-        if messages_on_line:
-            print(f"Actual messages on line {line}: {messages_on_line}")
-            for message in messages_on_line:
-                message_occurrence_counter[message] += 1
+            # If the message was not predicted because it's threshold was too high, update the threshold
+            if message not in predicted_messages_per_line[line]:
+                matching_scores = matching_scores_per_line_per_message[line]
                 if message in matching_scores:
-                    old_threshold = new_score_thresholds[message] if message in new_score_thresholds else model.score_thresholds[message]
-                    new_score_thresholds[message] = old_threshold * 0.8 + matching_scores[message] * 0.2 * 0.75
+                    # print(f"Matching score {message}: {matching_scores[message]}")
+                    self.model.update_score_threshold(message, matching_scores[message])
 
-        if predicted_message is not None:
-            print(f"Predicted message on line {line}: {predicted_message}")
-            # If the predicted message was "rejected" (so it was not actually placed there), raise the threshold
-            if predicted_message not in messages_on_line:
-                # TODO refine
-                new_score_thresholds[predicted_message] = 1.01 * matching_scores[predicted_message]
+        for message, count in message_occurrences_in_this_file.items():
+            old_average, old_file_count = self.average_occurrences_per_file.get(message, (0.0, 0))
+            new_file_count = old_file_count + 1
+            new_average = old_average + (count - old_average) / new_file_count
+            self.average_occurrences_per_file[message] = (new_average, new_file_count)
 
-        if predicted_message is None and not messages_on_line:
-            stats["TN"] += 1
-        else:
-            if predicted_message is not None:
-                if predicted_message in messages_on_line:
-                    stats["TP"] += 1
-                elif messages_on_line:
-                    stats["different_message"] += 1
-                else:
-                    stats["FP"] += 1
+        return messages_per_line
 
-            for m in messages_on_line:
-                if m != predicted_message and message_occurrence_counter[m] > 1:
+    def update_train(self, file: str, annotated_tree: AnnotatedTree):
+        self.train[file] = annotated_tree
+
+        if len(self.train) % self.retrain_every_n == 0:
+            print(f"Retraining model, train-set size: {len(self.train)}")
+            self.model.train(self.train, n_procs=8, thresholds=True)
+
+
+def calculate_statistics(result: List[Tuple[int, List[str], List[str]]], lines_with_subtree: List[int],
+                         stats: Counter, predictor: Predictor, seen_messages: Set[str]):
+    # TODO betere manier van metrics, zie classification report of confusion matrix https://medium.com/apprentice-journal/evaluating-multi-class-classifiers-12b2946e755b
+    # TODO multi-label classification evaluation: https://towardsdatascience.com/evaluating-multi-label-classifiers-a31be83da6ea
+    messages_in_file = Counter()
+    predictions_in_file = Counter()
+
+    non_empty_lines = [line for line, _, _ in result]
+    stats["TN"] += len(list(filter(lambda line: line not in non_empty_lines, lines_with_subtree)))
+
+    for line, messages, predicted_messages in result:
+        if messages:
+            messages_in_file.update(messages)
+
+        if predicted_messages:
+            predictions_in_file.update(predicted_messages)
+
+        correct_messages = set(predicted_messages).intersection(messages)
+        wrong_messages = set(predicted_messages).difference(messages)
+        unpredicted_messages = set(messages).difference(predicted_messages)
+
+        stats["TP"] += len(correct_messages)
+
+        if not messages:
+            stats["FP"] += len(wrong_messages)
+        elif unpredicted_messages:
+            for m in unpredicted_messages:
+                if m in seen_messages:
                     stats["FN"] += 1
-                    if any(m in model.patterns for m in messages_on_line):
+                    if m in predictor.model.patterns:
                         stats["filtered_FN"] += 1
 
-        if messages_on_line or predicted_message is not None:
-            results.append((line, messages_on_line, predicted_message))
-
-    model.score_thresholds.update(new_score_thresholds)
-
-    return results
+    seen_messages.update(messages_in_file.keys())
 
 
-def test_predictions(exercise_id: str):
-    analyzer = FeedbackAnalyzer()
-    analyzer.set_files(glob(f'{ROOT_DIR}/data/excercises/{exercise_id}/*.py'))
-
-    train, test = analyzer.create_train_test_set()
-    model = FeedbackModel()
-    model.train(train)
-
-    # FN zijn alle false negatives, dus ook van messages waarvoor er geen patronen zijn.
-    # Filtered FN zijn enkel de FN's van messages waarvoor er wel patronen zijn
-    stats = Counter({"FP": 0, "FN": 0, "TP": 0, "TN": 0, "filtered_FN": 0})
-    for file, tree in list(test.items()):
-        print(f"Results for file: {file}")
-        predictions_for_file(model, tree, stats)
-        # most_fitting_predictions_for_file(model, tree, stats)
-        print()
-
-    print(stats)
+def save_results(results: List[Tuple[str, List[Tuple[int, List[str], List[str]]]]], e_id: str):
+    with open(f'{ROOT_DIR}/output/predictor/fitting_results_{e_id}_2.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file, delimiter='|')
+        for file, line_results in results:
+            file_parts = file.split('/')
+            for line, actual_messages, predicted_messages in line_results:
+                writer.writerow([file_parts[-2], file_parts[-1], line, json.dumps(actual_messages), json.dumps(predicted_messages)])
 
 
 def test_simulate(exercise_id: str):
@@ -174,28 +145,41 @@ def test_simulate(exercise_id: str):
     analyzer = FeedbackAnalyzer()
     analyzer.set_files(glob(f'{ROOT_DIR}/data/excercises/{exercise_id}/*.py'))
 
-    model = FeedbackModel()
-
     results = []
 
-    train = {}
+    seen_messages = set()
+    predictor = Predictor()
+
     test = analyzer.analyze_files()
     files = analyzer.get_sorted_files()
     for i, file in enumerate(files):
         print(f"Results for file: {file}")
-        annotated_code = test.pop(file)
+        annotated_tree = test.pop(file)
 
-        result = predictions_for_file(model, annotated_code, stats)
+        predictions_per_line, scores, lines_with_subtree = predictor.predict(annotated_tree[0])
+        messages_per_line = predictor.process_actual_feedback(annotated_tree[1], predictions_per_line, scores)
+
+        result = []
+        for line in lines_with_subtree:
+            messages = list(messages_per_line[line])
+            if messages:
+                print(f"Actual messages on line {line}: {messages}")
+            predictions = predictions_per_line[line]
+            if predictions:
+                print(f"Predicted messages on line {line}: {predictions}")
+            if messages or predictions:
+                result.append((line, messages, list(predictions)))
+
+        calculate_statistics(result, lines_with_subtree, stats, predictor, seen_messages)
         results.append((file, result))
-        # most_fitting_predictions_for_file(model, annotated_code, stats)
 
-        train[file] = annotated_code
-        if i % 5 == 0:
-            # Only train again every 5 files
-            print(f"Retraining model, train-set size: {len(train)}")
-            model.train(train, thresholds=True)
+        predictor.update_train(file, annotated_tree)
 
-    save_results(results)
+        print()
+        # print(f"Current score thresholds: {predictor.model.score_thresholds}")
+        # print()
+
+    save_results(results, exercise_id)
 
     print(stats)
     print(f'False positive rate: {stats["FP"] / (stats["FP"] + stats["TN"])}')
@@ -204,5 +188,8 @@ def test_simulate(exercise_id: str):
 
 
 if __name__ == '__main__':
-    # test_predictions("505886137")
+    start = time.time()
     test_simulate("505886137")
+    print()
+    print(f"Total time: {time.time() - start}")
+
