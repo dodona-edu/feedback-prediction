97d43b1e50fd049ae7e45215aa29d882b7e89e0c
Kasper Demeyere <kasperdemeyere@hotmail.com>
2024-04-09 19:23:22 +0200

    fix possible edge case missing pattern, add test for it

--- src/tree_algorithms/subtree_matches.py
+++ src/tree_algorithms/subtree_matches.py
@@ -1,35 +1,34 @@
-from typing import List
+from typing import List, Tuple
 
 from custom_types import HorizontalTree
 from util import sequence_fully_contains_other_sequence
 
 
 def find_in_subtree(current_subtree: HorizontalTree, pattern: HorizontalTree, pattern_length: int, history: List,
-                    p_i: int, depth: int, depth_stack: List[int]) -> bool:
+                    p_i: int, depth: int, match_stack: List[Tuple[int, int, int]]) -> bool:
     local_history = []
     for i, item in enumerate(current_subtree):
         # We go up in the tree
         if item == -1:
             # If the depth of the last matched node is equal to what the depth will be after this iteration
-            if depth_stack and depth - 1 == depth_stack[-1]:
+            if match_stack and depth - 1 == match_stack[-1][0]:
                 # We go up past the last node
-                depth_stack.pop()
+                _, passed_p_i, local_history_index = match_stack.pop()
                 # If in the pattern, we are not supposed to go up
                 if pattern[p_i] != -1:
                     # Reset the pattern index
-                    p_i = 0
-                    # If we are past the first matched node, we can limit our future backtracking in local_history to a new subtree
-                    if not depth_stack:
-                        history.append((local_history, current_subtree[:i + 1]))
-                        local_history = []
-                    depth_stack = []
+                    p_i = passed_p_i
+                    # Since alternatives in the remainder of the tree will be explored by the remaining iterations,
+                    # we can limit our future backtracking in local_history to a subtree up until this point
+                    history.append((local_history[local_history_index:], current_subtree[:i + 1]))
+                    local_history = local_history[:local_history_index]
                 else:
                     p_i += 1
             depth -= 1
         else:
             if pattern[p_i] == item:
-                local_history.append((i + 1, depth + 1, depth_stack[:], p_i))
-                depth_stack.append(depth)
+                local_history.append((i + 1, depth + 1, match_stack[:], p_i))
+                match_stack.append((depth, p_i, len(local_history) - 1))
                 p_i += 1
 
             depth += 1
@@ -83,6 +82,8 @@ def subtree_matches(subtree: HorizontalTree, pattern: HorizontalTree) -> bool:
     True
     >>> subtree_matches((4, 0, 1, 3, 1, -1, 2, -1, -1, 2, -1, -1, 2, -1, -1, 5, -1), (0, 1, -1, 2, -1, 2, -1, 2, -1))
     True
+    >>> subtree_matches(('masker', 'block', 'return_statement', 'subscript', 'call', 'attribute', 'call', 'attribute', '"{0:b}"', -1, 'format', -1, -1, 'argument_list', 'int1', -1, -1, -1, 'zfill', -1, -1, 'argument_list', 'call', 'zop', -1, 'argument_list', 'str1', -1, -1, -1, -1, 'call', 'len', -1, 'argument_list', -1, -1, -1, 'slice', '-', '1', -1, -1, -1, -1, -1, -1), ('call', 'call', 'len', -1, 'argument_list'))
+    True
     >>> subtree_matches(('expression_statement', 'assignment', 'uitgang', -1, '=', -1, 'list', '[', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ',', -1, 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'range', -1, 'argument_list', '(', -1, 'integer', -1, ',', -1, 'integer', -1, ')', -1, -1, -1, ')', -1, -1, -1, ']', -1, -1, -1), ('list', 'call', 'range', -1, '(', -1, '('))
     False
     >>> subtree_matches(('expression_statement', 'augmented_assignment', 'output', -1, '+=', -1, 'subscript', 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'attribute', 'attribute', 'self', -1, '.', -1, 'codec', -1, -1, '.', -1, 'keys', -1, -1, 'argument_list', '(', -1, ')', -1, -1, -1, ')', -1, -1, -1, '[', -1, 'call', 'attribute', 'call', 'list', -1, 'argument_list', '(', -1, 'call', 'attribute', 'attribute', 'self', -1, '.', -1, 'codec', -1, -1, '.', -1, 'values', -1, -1, 'argument_list', '(', -1, ')', -1, -1, -1, ')', -1, -1, -1, '.', -1, 'index', -1, -1, 'argument_list', '(', -1, 'element', -1, ')', -1, -1, -1, ']', -1, -1, -1), ('call', 'attribute', '.', -1, -1, '(', -1, 'self'))
@@ -114,10 +115,10 @@ def subtree_matches(subtree: HorizontalTree, pattern: HorizontalTree) -> bool:
     while not result and history:
         to_explore, to_explore_subtree = history.pop()
         while not result and to_explore:
-            start, depth, depth_stack, p_i = to_explore.pop()
+            start, depth, match_stack, p_i = to_explore.pop()
             new_subtree = to_explore_subtree[start:]
-            # Subtree needs to contain all items from pattern
+            # The new_subtree needs to be at least as long as the remaining pattern and contain all items from the remaining pattern
             if pattern_length - p_i <= len(new_subtree) and sequence_fully_contains_other_sequence(new_subtree, pattern[p_i:]):
-                result = find_in_subtree(new_subtree, pattern, pattern_length, history, p_i, depth, depth_stack)
+                result = find_in_subtree(new_subtree, pattern, pattern_length, history, p_i, depth, match_stack)
 
     return result
